84a9319ae63b845beba320e06f8728a961e86ac3
pegasas <pegasashjy@gmail.com>
2024-04-29 02:08:34 +0800

    2024042902

--- java-exp/web/mybatis-plus/src/main/java/com/pegasas/exp/Main.java
+++ java-exp/web/mybatis-plus/src/main/java/com/pegasas/exp/Main.java
@@ -1,7 +1,37 @@
 package com.pegasas.exp;
 
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://192.168.101.150:3306/cms?allowPublicKeyRetrieval=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8&useUnicode=true");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        dataSource.setIdleTimeout(60000);
+        dataSource.setAutoCommit(true);
+        dataSource.setMaximumPoolSize(5);
+        dataSource.setMinimumIdle(1);
+        dataSource.setMaxLifetime(60000 * 10);
+        dataSource.setConnectionTestQuery("SELECT 1");
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("show table status");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                String name = metaData.getColumnLabel(i);
+                String field = resultSet.getString(i);
+                System.out.printf("%s:%s\t",name,field);
+            }
+            System.out.println();
+        }
     }
 }
