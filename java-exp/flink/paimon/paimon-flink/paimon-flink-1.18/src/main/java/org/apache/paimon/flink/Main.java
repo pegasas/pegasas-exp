6e833d42f49d7efcb3a0768f03e23a9b0d4fde5e
pegasas <pegasashjy@gmail.com>
2024-05-03 21:10:07 +0800

    2021050301

--- java-exp/flink/paimon/paimon-flink/paimon-flink-1.18/src/main/java/org/apache/paimon/flink/Main.java
+++ java-exp/flink/paimon/paimon-flink/paimon-flink-1.18/src/main/java/org/apache/paimon/flink/Main.java
@@ -10,9 +10,17 @@ public class Main {
 
         tableEnv.executeSql("CREATE CATALOG my_catalog WITH (\n" +
                 "    'type'='paimon',\n" +
-                "    'warehouse'='/home/huangjunyao/paimon'\n" +
+                "    'warehouse'='file:///home/huangjunyao/paimon'\n" +
                 ")");
         tableEnv.executeSql("USE CATALOG my_catalog");
+        tableEnv.executeSql("CREATE TABLE Orders (\n" +
+                "    order_number BIGINT,\n" +
+                "    price        DECIMAL(32,2),\n" +
+                "    buyer        ROW<first_name STRING, last_name STRING>,\n" +
+                "    order_time   TIMESTAMP(3)\n" +
+                ") WITH (\n" +
+                "  'connector' = 'datagen'\n" +
+                ")");
 //        tableEnv.executeSql("USE CATALOG my_catalog;").print();
     }
 }
