1414a7fec6dd72d2b69ca537acbb519e6eb66cc5
pegasas <pegasashjy@gmail.com>
2024-06-17 20:51:51 -0400

    cuda

--- cuda-exp/main.cu
+++ cuda-exp/main.cu
@@ -1,19 +1,51 @@
-#include <stdio.h>
 #include <iostream>
-__global__ void add(int a, int b){
-    int c = a + b;
-    int d = c + a;
-    printf("c: %d\n", c);
+#include <stdlib.h>
+#include <sys/time.h>
+
+
+const int ROWS = 1024;
+const int COLS = 1024;
+
+using namespace std;
+
+void matrix_mul_cpu(float* M, float* N, float* P, int width)
+{
+    for(int i=0;i<width;i++)
+        for(int j=0;j<width;j++)
+        {
+            float sum = 0.0;
+            for(int k=0;k<width;k++)
+            {
+                float a = M[i*width+k];
+                float b = N[k*width+j];
+                sum += a*b;
+            }
+            P[i*width+j] = sum;
+        }
 }
 
-void test_add(int a, int b){
-    add<<<1, 1>>>(a, b);
-    cudaDeviceReset(); // 同步设备函数的结果
-}
+int main()
+{
+    struct timeval start, end;
+    gettimeofday( &start, NULL );
+    float *A, *B, *C;
+    int total_size = ROWS*COLS*sizeof(float);
+    A = (float*)malloc(total_size);
+    B = (float*)malloc(total_size);
+    C = (float*)malloc(total_size);
 
-int main() {
-    int a = 1, b = 2;
-    test_add(a, b);
-    std::cout << "Hello, World!" << std::endl;
+    //CPU一维数组初始化
+    for(int i=0;i<ROWS*COLS;i++)
+    {
+        A[i] = 80.0;
+        B[i] = 20.0;
+    }
+
+    matrix_mul_cpu(A, B, C, COLS);
+
+    gettimeofday( &end, NULL );
+    int timeuse = 1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec;
+    cout << "total time is " << timeuse/1000 << "ms" <<endl;
+
     return 0;
 }
